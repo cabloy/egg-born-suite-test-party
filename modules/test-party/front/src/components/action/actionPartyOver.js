fc9afafa49be805c06769ac9e9f219c44ec559ca
zhennann <zhen.nann@icloud.com>
2022-08-06 20:53:21 +0800

    feat: dialog.confirm

--- modules/test-party/front/src/components/action/actionPartyOver.js
+++ modules/test-party/front/src/components/action/actionPartyOver.js
@@ -3,6 +3,7 @@ export default {
     async _onActionPartyOver() {
       const { ctx, item } = this.$props;
       const key = { atomId: item.atomId, itemId: item.itemId };
+      await ctx.$view.dialog.confirm();
       await ctx.$api.post('/test/party/party/over', { key });
       ctx.$meta.eventHub.$emit('atom:action', { key, action: { name: 'save' } });
       return true;
