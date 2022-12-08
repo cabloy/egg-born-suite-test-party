1fb48942fd8a6c81e91f7009684b0ea5a99c5e44
zhennann <zhen.nann@icloud.com>
2022-12-08 15:27:31 +0800

    feat: areaScope test

--- modules/test-party/backend/src/controller/test/feat/areaScope.js
+++ modules/test-party/backend/src/controller/test/feat/areaScope.js
@@ -22,6 +22,7 @@ module.exports = app => {
       //   9. 清理测试数据：areaScopeTest001 / rose权限
 
       // 准备数据
+      const userRoot = await this.ctx.bean.user.get({ userName: 'root' });
       const userJimmy = await this.ctx.bean.user.get({ userName: 'jimmy' });
       const userRose = await this.ctx.bean.user.get({ userName: 'rose' });
       const roleFriend = await this.ctx.bean.role.parseRoleName({ roleName: 'friend' });
@@ -59,26 +60,41 @@ module.exports = app => {
       assert.equal(party.atomName, atomName);
 
       // 2. rose: select/read，均查不到areaScopeTest001
-      const checkItems = await this.ctx.bean.atom.select({
-        user: userRose,
-        options: {
-          where: { atomName },
-          stage: 'formal',
-        },
-      });
-      assert.equal(checkItems.length, 0);
+      await this._checkRight({ user: userRose, party, shouldExists: false, step: 2 });
 
-      const checkRightRead = await this.ctx.bean.atom.checkRightRead({
-        atom: { id: partyKeyFormal.atomId },
-        user: userRose,
+      // 3. rose: 分配权限：可以查看角色：friend的权限，区域为空
+      const roleRightId = await this.ctx.bean.role.addRoleRight({
+        roleId: roleFriend.id,
+        atomClassId: party.atomClassId,
+        action: 2, // read
+        scope: [roleFriend.id],
+        user: userRoot,
       });
-      assert.equal(!!checkRightRead, false);
+      await this._checkRight({ user: userRose, party, shouldExists: true, step: 3 });
 
       // 9. 清理测试数据：areaScopeTest001 / rose权限
       await this.ctx.bean.atom.delete({ key: partyKeyFormal, user: userJimmy });
-
+      await this.ctx.bean.role.deleteRoleRight({ roleId: roleFriend.id, roleRightId, user: userRoot });
       // ok
       this.ctx.success();
+    }
+
+    async _checkRight({ user, party, shouldExists, step }) {
+      const stepText = `step: ${step}`;
+      const checkItems = await this.ctx.bean.atom.select({
+        user,
+        options: {
+          where: { atomName: party.atomName },
+          stage: 'formal',
+        },
+      });
+      assert.equal(checkItems.length > 0, shouldExists, stepText);
+
+      const checkRightRead = await this.ctx.bean.atom.checkRightRead({
+        atom: { id: party.atomId },
+        user,
+      });
+      assert.equal(!!checkRightRead, shouldExists, stepText);
     }
   }
 
