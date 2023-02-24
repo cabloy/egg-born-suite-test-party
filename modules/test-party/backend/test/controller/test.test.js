1afd74cb1127dc83747c7c21017e36303223998d
zhennann <zhen.nann@icloud.com>
2023-02-24 16:11:37 +0800

    chore: version 4.8.28

--- modules/test-party/backend/test/controller/test.test.js
+++ modules/test-party/backend/test/controller/test.test.js
@@ -24,7 +24,7 @@ describe('action: atom: party', () => {
       .httpRequest()
       .post(mockUrl('/a/base/atom/create'))
       .send({
-        atomClass: { module: atomClassModule, atomClassName, atomClassIdParent: 0 },
+        atomClass: { module: atomClassModule, atomClassName },
       });
     assert(result.body.code === 0);
     const keyDraft = result.body.data;
