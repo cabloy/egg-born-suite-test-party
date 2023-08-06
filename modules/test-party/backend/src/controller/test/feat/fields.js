16359132a55df39dc905b5dacb77752650d26c1d
zhennann <zhen.nann@icloud.com>
2023-08-06 11:44:28 +0800

    feat: _parseSchema_mode_allowSpecificFields_2/_parseSchema_mode_allowSpecificFields_3

--- modules/test-party/backend/src/controller/test/feat/fields.js
+++ modules/test-party/backend/src/controller/test/feat/fields.js
@@ -12,6 +12,20 @@ const __testData_allowSpecificFields_1 = {
   basic: { read: true, write: true },
   fields: ['atomName', { name: 'detailsAmount', read: true, write: false }],
 };
+const __testData_allowSpecificFields_2 = {
+  mode: 'allowSpecificFields',
+  basic: { read: true, write: false },
+  fields: ['atomName', { name: 'detailsAmount', read: true, write: true }, 'details'],
+};
+const __testData_allowSpecificFields_3 = {
+  mode: 'allowSpecificFields',
+  basic: { read: false, write: false },
+  fields: [
+    'atomName',
+    { name: 'detailsAmount', read: true, write: true },
+    { name: 'details', read: true, write: false },
+  ],
+};
 
 module.exports = app => {
   // const moduleInfo = app.meta.mockUtil.parseInfoFromPackage(__dirname);
@@ -31,6 +45,10 @@ module.exports = app => {
       await this._parseSchema_mode_allowAllFieldsReadWrite({ atomClass });
       // mode: allowSpecificFields_1
       await this._parseSchema_mode_allowSpecificFields_1({ atomClass });
+      // mode: allowSpecificFields_2
+      await this._parseSchema_mode_allowSpecificFields_2({ atomClass });
+      // mode: allowSpecificFields_3
+      await this._parseSchema_mode_allowSpecificFields_3({ atomClass });
       // ok
       this.ctx.success();
     }
@@ -66,6 +84,28 @@ module.exports = app => {
       assert.equal(schemaBase.schema.properties.atomName, undefined);
       assert.equal(schemaBase.schema.properties.detailsAmount.ebReadOnly, true);
       assert.equal(schemaBase.schema.properties.details.ebReadOnly, undefined);
+    }
+
+    // mode: allowSpecificFields_2
+    async _parseSchema_mode_allowSpecificFields_2({ atomClass }) {
+      const schemaBase = await this.ctx.bean.fields.parseSchema({
+        atomClass,
+        fieldsRight: __testData_allowSpecificFields_2,
+      });
+      assert.equal(schemaBase.schema.properties.atomName, undefined);
+      assert.equal(schemaBase.schema.properties.detailsAmount.ebReadOnly, false);
+      assert.equal(schemaBase.schema.properties.details, undefined);
+    }
+
+    // mode: allowSpecificFields_3
+    async _parseSchema_mode_allowSpecificFields_3({ atomClass }) {
+      const schemaBase = await this.ctx.bean.fields.parseSchema({
+        atomClass,
+        fieldsRight: __testData_allowSpecificFields_3,
+      });
+      assert.equal(schemaBase.schema.properties.atomName, undefined);
+      assert.equal(schemaBase.schema.properties.detailsAmount.ebReadOnly, false);
+      assert.equal(schemaBase.schema.properties.details.ebReadOnly, true);
     }
   }
   return FieldsController;
