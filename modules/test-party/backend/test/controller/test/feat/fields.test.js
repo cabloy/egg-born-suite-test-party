55152f7e6fe522b2d33e6a642de918e556bd241b
zhennann <zhen.nann@icloud.com>
2023-08-07 08:42:57 +0800

    feat: _parseSchema_mode_custom_object

--- modules/test-party/backend/test/controller/test/feat/fields.test.js
+++ modules/test-party/backend/test/controller/test/feat/fields.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe.only('test/controller/test/feat/fields.test.js', () => {
+describe('test/controller/test/feat/fields.test.js', () => {
   it('action:fields:parseSchema', async () => {
     const result = await app.httpRequest().post(mockUrl('test/feat/fields/parseSchema'));
     assert.equal(result.body.code, 0);
