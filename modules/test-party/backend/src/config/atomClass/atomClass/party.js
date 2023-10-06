f9659b9407a81d233cb29e39bfb898e466a54b6a
zhennann <zhen.nann@icloud.com>
2023-10-06 11:12:48 +0800

    feat: base_getDialogTitle

--- modules/test-party/backend/src/config/atomClass/atomClass/party.js
+++ modules/test-party/backend/src/config/atomClass/atomClass/party.js
@@ -47,6 +47,7 @@ module.exports = app => {
           transaction: true, // default is true
           // atomState: 1, // over
           dialog: {
+            title: null,
             confirm: true, // default is true
             confirmText: null,
             toast: true, // default is true
