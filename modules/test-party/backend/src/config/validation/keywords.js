186f4414cccb35f5092359d7bf7b289255f217c5
zhennann <zhen.nann@icloud.com>
2023-10-10 19:46:35 +0800

    feat: remove appendPrefix

--- modules/test-party/backend/src/config/validation/keywords.js
+++ modules/test-party/backend/src/config/validation/keywords.js
@@ -1,21 +1,9 @@
 const languages = require('./keyword/languages.js');
 
-function appendPrefix(keywords) {
-  const result = {};
-  for (const key in keywords) {
-    if (key.indexOf('x-') === 0) {
-      result[key] = keywords[key];
-    } else {
-      result[`x-${key}`] = keywords[key];
-    }
-  }
-  return result;
-}
-
 module.exports = app => {
   const keywords = {};
   // product
-  Object.assign(keywords, appendPrefix(languages(app)));
+  Object.assign(keywords, languages(app));
   // ok
   return keywords;
 };
