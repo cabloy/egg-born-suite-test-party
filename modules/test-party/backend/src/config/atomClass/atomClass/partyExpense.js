882311228199e6e685f51315e178ebe10e23554c
zhennann <zhen.nann@icloud.com>
2023-10-21 17:06:43 +0800

    feat: partyExpenseOtherTest2

--- modules/test-party/backend/src/config/atomClass/atomClass/partyExpense.js
+++ modules/test-party/backend/src/config/atomClass/atomClass/partyExpense.js
@@ -29,6 +29,57 @@ module.exports = app => {
       clone: {},
       moveUp: {},
       moveDown: {},
+      partyExpenseOtherTest2: {
+        code: 101,
+        title: 'OtherTest2',
+        actionModule: 'a-base',
+        actionComponent: 'action',
+        icon: { f7: '::dot' },
+        rightInherit: 'write',
+        rightInheritStage: 'draft',
+        mode: 'edit',
+        directShowOnList: false,
+        directShowOnItem: false,
+        params: {
+          transaction: true, // default is true
+          dialog: {
+            title: null,
+            confirm: true, // default is true
+            confirmText: null,
+            toast: true, // default is true
+            toastText: null,
+          },
+          form: {
+            mode: 'edit', // edit/view
+            fieldsRight: {
+              mode: 'allowSpecificFields',
+              basic: { read: false, write: false },
+              fields: [
+                { name: 'price', read: true, write: true },
+                { name: 'quantity', read: true, write: true },
+                { name: 'amount', read: true, write: true },
+              ],
+            },
+          },
+          actionAfter: {
+            sameAs: 'write',
+            commands: null, //
+          },
+        },
+      },
+      partyExpenseOtherTest3: {
+        code: 102,
+        title: 'OtherTest3',
+        actionModule: 'a-base',
+        actionComponent: 'action',
+        icon: { f7: '::dot' },
+        rightInherit: 'write',
+        rightInheritStage: 'draft',
+        mode: 'edit',
+        directShowOnList: false,
+        directShowOnItem: false,
+        // enableOnFormAction: 'partyExpenseOtherTest2',
+      },
     },
     validator: 'partyExpense',
   };
