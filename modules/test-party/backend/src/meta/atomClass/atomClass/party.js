bb9501108f370060ba3312aee60a7c02d45d0604
zhennann <zhen.nann@icloud.com>
2023-10-24 20:04:39 +0800

    feat: validate

--- modules/test-party/backend/src/meta/atomClass/atomClass/party.js
+++ modules/test-party/backend/src/meta/atomClass/atomClass/party.js
@@ -137,7 +137,7 @@ module.exports = app => {
               mode: 'custom',
               custom: {
                 module: 'test-party',
-                schema: 'partyOverBulk',
+                validator: 'partyOverBulk',
               },
             },
           },
