b2cf5947545e87df858c3586cd6792c798fea6a2
zhennann <zhen.nann@icloud.com>
2023-11-03 17:04:53 +0800

    refactor: atom bean

--- modules/test-party/backend/src/bean/atom.partyExpense.js
+++ modules/test-party/backend/src/bean/atom.partyExpense.js
@@ -10,7 +10,7 @@ module.exports = ctx => {
     }
 
     async default({ atomClass, item, options, user }) {
-      // party default
+      // partyExpense default
       const data = await this.model.default();
       // super
       return await super.default({ atomClass, data, item, options, user });
