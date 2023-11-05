3437b401f4b561cd55b79972b2cfd446cd278203
zhennann <zhen.nann@icloud.com>
2023-11-05 15:58:31 +0800

    feat: test demo

--- modules/test-party/backend/src/controller/test/demo.js
+++ modules/test-party/backend/src/controller/test/demo.js
@@ -1,8 +1,14 @@
 module.exports = app => {
   class DemoController extends app.Controller {
     async demo() {
-      this.ctx.success('s');
+      const jsFile = await this._prepareJSFile();
+      const DemoFn = app.meta.util.requireDynamic(jsFile);
+      const demo = new (DemoFn(this.ctx))();
+      const result = await demo.execute();
+      this.ctx.success(result);
     }
+
+    async _prepareJSFile() {}
   }
 
   return DemoController;
