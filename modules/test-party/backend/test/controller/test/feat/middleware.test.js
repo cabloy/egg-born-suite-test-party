13cfefdc2f5b1fa35a90de1756d8753a693bcefb
zhennann <zhen.nann@icloud.com>
2023-11-12 14:25:00 +0800

    feat: catchError

--- modules/test-party/backend/test/controller/test/feat/middleware.test.js
+++ modules/test-party/backend/test/controller/test/feat/middleware.test.js
@@ -1,6 +1,6 @@
 const { app, mockUrl, mockInfo, assert } = require('egg-born-mock')(__dirname);
 
-describe('test/controller/test/feat/middleware.test.js', () => {
+describe.only('test/controller/test/feat/middleware.test.js', () => {
   it('action:interception', async () => {
     // ctx
     const ctx = await app.mockCtx();
@@ -18,15 +18,22 @@ describe('test/controller/test/feat/middleware.test.js', () => {
     assert.equal(data, 3);
 
     // fail
-    try {
-      await ctx.meta.util.performAction({
-        innerAccess: false,
-        method: 'post',
-        url: mockUrl('test/feat/middleware/interception', false),
-      });
-    } catch (err) {
-      assert.equal(err.code, 1002);
-    }
+    await ctx.meta.mockUtil.catchError(
+      async function () {
+        return await ctx.meta.util.performAction({
+          innerAccess: false,
+          method: 'post',
+          url: mockUrl('test/feat/middleware/interception', false),
+          body: {
+            // a: '1',
+            // b: '2',
+          },
+        });
+      },
+      async function (err) {
+        assert.equal(err.code, 1002);
+      }
+    );
   });
 
   it('action:restructuring', async () => {
