1438a0056ff43409ece60b44659e24d729f7ba44
zhennann <zhen.nann@icloud.com>
2023-12-15 20:54:35 +0800

    refactor: controllers

--- modules/test-partymonkey-monkey/backend/src/controller/monkeyer.js
+++ modules/test-partymonkey-monkey/backend/src/controller/monkeyer.js
@@ -1,14 +1,10 @@
-module.exports = app => {
-  const moduleInfo = module.info;
-  class MonkeyerController extends app.Controller {
-    async test() {
-      const config = this.ctx.config.module('test-party');
-      this.ctx.success({
-        moduleName: moduleInfo.relativeName,
-        monkeyed: config.monkeyed,
-      });
-    }
+const moduleInfo = module.info;
+module.exports = class MonkeyerController {
+  async test() {
+    const config = this.ctx.config.module('test-party');
+    this.ctx.success({
+      moduleName: moduleInfo.relativeName,
+      monkeyed: config.monkeyed,
+    });
   }
-
-  return MonkeyerController;
 };
--- modules/test-wechat/backend/src/controller/test.js
+++ modules/test-wechat/backend/src/controller/test.js
@@ -1,19 +1,16 @@
-module.exports = app => {
-  class TestController extends app.Controller {
-    async getOpenid() {
-      const res = await this.service.test.getOpenid({
-        user: this.ctx.state.user.op,
-      });
-      this.ctx.success(res);
-    }
-
-    async getOpenidMini() {
-      const res = await this.service.test.getOpenidMini({
-        providerScene: this.ctx.request.body.providerScene,
-        user: this.ctx.state.user.op,
-      });
-      this.ctx.success(res);
-    }
+module.exports = class TestController {
+  async getOpenid() {
+    const res = await this.ctx.service.test.getOpenid({
+      user: this.ctx.state.user.op,
+    });
+    this.ctx.success(res);
   }
-  return TestController;
+
+  async getOpenidMini() {
+    const res = await this.ctx.service.test.getOpenidMini({
+      providerScene: this.ctx.request.body.providerScene,
+      user: this.ctx.state.user.op,
+    });
+    this.ctx.success(res);
+  }
 };
