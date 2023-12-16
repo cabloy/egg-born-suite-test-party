04d6618b79108324c291ed8112351a51e5c957cf
zhennann <zhen.nann@icloud.com>
2023-12-16 20:10:49 +0800

    refactor: monkey

--- modules/test-partymonkey-monkey/backend/src/monkey.js
+++ modules/test-partymonkey-monkey/backend/src/monkey.js
@@ -1,38 +1,34 @@
-module.exports = app => {
-  // eslint-disable-next-line
-  const moduleInfo = module.info;
+const moduleInfo = module.info;
 
-  function monkeyRoute(module, routePath, routeController) {
-    const route = module.main.routes.find(item => item.path === routePath);
-    if (route) {
-      route.controller = routeController;
-    }
+function monkeyRoute(module, routePath, routeController) {
+  const route = module.main.routes.find(item => item.path === routePath);
+  if (route) {
+    route.controller = routeController;
   }
+}
 
-  function monkeyConfig(module, config) {
-    config.monkeyed = true;
+function monkeyConfig(module, config) {
+  config.monkeyed = true;
+}
+
+module.exports = class Monkey {
+  moduleLoading({ /* moduleSelf,*/ module }) {
+    if (module.info.relativeName !== 'test-party') return;
+    // route
+    monkeyRoute(module, 'test/monkey/monkeyee/test', {
+      module: moduleInfo.relativeName,
+      name: 'monkeyer',
+    });
   }
-
-  const monkey = {
-    moduleLoading({ /* moduleSelf,*/ module }) {
-      if (module.info.relativeName !== 'test-party') return;
-      // route
-      monkeyRoute(module, 'test/monkey/monkeyee/test', {
-        module: moduleInfo.relativeName,
-        name: 'monkeyer',
-      });
-    },
-    moduleLoaded(/* { moduleSelf, module }*/) {
-      // do nothing
-    },
-    configLoaded({ /* moduleSelf,*/ module, config }) {
-      if (module.info.relativeName !== 'test-party') return;
-      // config
-      monkeyConfig(module, config);
-    },
-    metaLoaded(/* { moduleSelf, module, meta }*/) {
-      // do nothing
-    },
-  };
-  return monkey;
+  moduleLoaded(/* { moduleSelf, module }*/) {
+    // do nothing
+  }
+  configLoaded({ /* moduleSelf,*/ module, config }) {
+    if (module.info.relativeName !== 'test-party') return;
+    // config
+    monkeyConfig(module, config);
+  }
+  metaLoaded(/* { moduleSelf, module, meta }*/) {
+    // do nothing
+  }
 };
