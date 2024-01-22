7f060680c3c61d8734f210003b571eefec79f6f8
zhennann <zhen.nann@icloud.com>
2024-01-22 21:01:07 +0800

    feat: suite

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import 'cabloy-module-api-test-dingtalk';
+import 'cabloy-module-api-test-hostsimple-sync';
+import 'cabloy-module-api-test-localeone';
+import 'cabloy-module-api-test-localetwo';
+import 'cabloy-module-api-test-party';
+import 'cabloy-module-api-test-partymonkey-monkey';
+import 'cabloy-module-api-test-wechat';
+import 'cabloy-module-api-test-wxwork';
