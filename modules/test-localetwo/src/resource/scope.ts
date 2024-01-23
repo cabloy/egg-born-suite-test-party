77148e0b20b015e2528f1100bf17a6d310e58b53
zhennann <zhen.nann@icloud.com>
2024-01-23 21:40:56 +0800

    feat: resource scope

---
+++ modules/test-localetwo/src/resource/scope.ts
@@ -0,0 +1,23 @@
+import { BeanScopeBase, Scope, TypeModuleResource } from '@cabloy/core';
+import { IModuleLocal } from './locals.js';
+import { IModuleModel } from './models.js';
+import { config, Errors, locales, constants } from '../config/index.js';
+
+@Scope()
+export class ScopeModuleTestLocaletwo extends BeanScopeBase {}
+
+export interface ScopeModuleTestLocaletwo
+  extends TypeModuleResource<
+    IModuleLocal,
+    IModuleModel,
+    typeof config,
+    typeof Errors,
+    typeof locales,
+    typeof constants
+  > {}
+
+declare module '@cabloy/core' {
+  export interface IBeanScopeRecord {
+    'test-localetwo': ScopeModuleTestLocaletwo;
+  }
+}
---
+++ modules/test-partymonkey-monkey/src/resource/scope.ts
@@ -0,0 +1,23 @@
+import { BeanScopeBase, Scope, TypeModuleResource } from '@cabloy/core';
+import { IModuleLocal } from './locals.js';
+import { IModuleModel } from './models.js';
+import { config, Errors, locales, constants } from '../config/index.js';
+
+@Scope()
+export class ScopeModuleTestPartymonkey extends BeanScopeBase {}
+
+export interface ScopeModuleTestPartymonkey
+  extends TypeModuleResource<
+    IModuleLocal,
+    IModuleModel,
+    typeof config,
+    typeof Errors,
+    typeof locales,
+    typeof constants
+  > {}
+
+declare module '@cabloy/core' {
+  export interface IBeanScopeRecord {
+    'test-partymonkey': ScopeModuleTestPartymonkey;
+  }
+}
