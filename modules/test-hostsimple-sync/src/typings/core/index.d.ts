a0be7896c74e024920cf14490c9b7c18cc4dc4c6
zhennann <zhen.nann@icloud.com>
2024-01-28 22:13:59 +0800

    feat: typings

--- modules/test-hostsimple-sync/src/typings/core/index.d.ts
+++ modules/test-hostsimple-sync/src/typings/core/index.d.ts
@@ -1,8 +1,1 @@
-import 'cabloy-module-api-a-validation';
-import 'cabloy-module-api-a-instance';
-import 'cabloy-module-api-a-fields';
-import 'cabloy-module-api-a-summer';
-import 'cabloy-module-api-a-stats';
-import 'cabloy-module-api-a-debug';
-import 'cabloy-module-api-a-dict';
-import 'cabloy-module-api-a-version';
+import 'cabloy-module-api-a-base';
