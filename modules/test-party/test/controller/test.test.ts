74d5837f2dc4385b2a3cdace8041dcade2912b9b
zhennann <zhen.nann@icloud.com>
2024-02-03 22:58:46 +0800

    feat: mock

--- modules/test-party/test/controller/test.test.ts
+++ modules/test-party/test/controller/test.test.ts
@@ -3,74 +3,66 @@ import { app, mockUrl, mockInfo, assert } from 'egg-born-mock';
 
 describe.only('action: atom: party', () => {
   it('[atom]', async () => {
-    const url = mockUrl('test/atom/all', false);
-    const moduleName = mockInfo().relativeName;
-    console.log(url, moduleName);
+    // ctx
+    const ctx = await app.meta.mockUtil.mockCtx();
+
+    // atomClass info
+    const atomClassModule = mockInfo().relativeName;
+    const atomClassName = 'party';
+    const atomClass = { module: atomClassModule, atomClassName };
+
+    // login as root
+    await ctx.meta.mockUtil.login({ auth: 'root' });
+
+    // create
+    const keyDraft = await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/write',
+      body: {
+        atomClass,
+        item: {
+          atomName: 'party demo',
+          personCount: 3,
+        },
+      },
+    });
+    assert(!!keyDraft);
+
+    // submit
+    let data = await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/submit',
+      body: {
+        key: keyDraft,
+        atomClass,
+      },
+    });
+    const keyFormal = data.formal.key;
+    assert(!!keyFormal);
+
+    // read
+    data = await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/read',
+      body: {
+        key: keyFormal,
+        atomClass,
+      },
+    });
+    assert(!!data);
+
+    // delete
+    await ctx.meta.util.performAction({
+      innerAccess: false,
+      method: 'post',
+      url: '/a/base/atom/delete',
+      body: {
+        key: keyFormal,
+        atomClass,
+      },
+    });
   });
 });
-// return;
-// describe.only('action: atom: party', () => {
-//   it('[atom]', async () => {
-//     // ctx
-//     const ctx = await app.meta.mockUtil.mockCtx();
-
-//     // atomClass info
-//     const atomClassModule = mockInfo().relativeName;
-//     const atomClassName = 'party';
-//     const atomClass = { module: atomClassModule, atomClassName };
-
-//     // login as root
-//     await ctx.meta.mockUtil.login({ auth: 'root' });
-
-//     // create
-//     const keyDraft = await ctx.meta.util.performAction({
-//       innerAccess: false,
-//       method: 'post',
-//       url: '/a/base/atom/write',
-//       body: {
-//         atomClass,
-//         item: {
-//           atomName: 'party demo',
-//           personCount: 3,
-//         },
-//       },
-//     });
-//     assert(!!keyDraft);
-
-//     // submit
-//     let data = await ctx.meta.util.performAction({
-//       innerAccess: false,
-//       method: 'post',
-//       url: '/a/base/atom/submit',
-//       body: {
-//         key: keyDraft,
-//         atomClass,
-//       },
-//     });
-//     const keyFormal = data.formal.key;
-//     assert(!!keyFormal);
-
-//     // read
-//     data = await ctx.meta.util.performAction({
-//       innerAccess: false,
-//       method: 'post',
-//       url: '/a/base/atom/read',
-//       body: {
-//         key: keyFormal,
-//         atomClass,
-//       },
-//     });
-//     assert(!!data);
-
-//     // delete
-//     await ctx.meta.util.performAction({
-//       innerAccess: false,
-//       method: 'post',
-//       url: '/a/base/atom/delete',
-//       body: {
-//         key: keyFormal,
-//         atomClass,
-//       },
-//     });
-//   });
-// });
