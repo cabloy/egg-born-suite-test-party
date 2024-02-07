12c907e4e2ab469ea970ddcb5a814ae8f255b2ae
zhennann <zhen.nann@icloud.com>
2024-02-07 08:04:06 +0800

    feat: types

--- modules/test-partymonkey-monkey/src/index.ts
+++ modules/test-partymonkey-monkey/src/index.ts
@@ -2,7 +2,8 @@ export * from './config/index.js';
 export * from './resource/index.js';
 export * from './meta.js';
 export * from './routes.js';
+export * from './types.js';
 
 export * from './monkey.js';
 
-import './types.js';
+import './typings.js';
