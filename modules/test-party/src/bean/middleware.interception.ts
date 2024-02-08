524aca21b04e60620e7d87c2f884cefac9e56ca6
zhennann <zhen.nann@icloud.com>
2024-02-08 11:42:30 +0800

    feat: MiddlewareTestInterception

--- modules/test-party/src/bean/middleware.interception.ts
+++ modules/test-party/src/bean/middleware.interception.ts
@@ -1,7 +1,7 @@
 import { Bean, BeanBase } from '@cabloy/core';
 
 @Bean({ scene: 'middleware' })
-export class MiddlewareInterception extends BeanBase {
+export class MiddlewareTestInterception extends BeanBase {
   async execute(_options, next) {
     const { a, b } = this.ctx.request.body;
     if (a === undefined || b === undefined) return this.ctx.throw(1002); // 1002: 'Incomplete Parameters'
--- modules/test-party/src/bean/middleware.restructuring.ts
+++ modules/test-party/src/bean/middleware.restructuring.ts
@@ -1,7 +1,7 @@
 import { Bean, BeanBase } from '@cabloy/core';
 
 @Bean({ scene: 'middleware' })
-export class MiddlewareRestructuring extends BeanBase {
+export class MiddlewareTestRestructuring extends BeanBase {
   async execute(_options, next) {
     const { a, b } = this.ctx.request.body;
     this.ctx.request.body.a = parseInt(a);
