d9b0ddd052c482cfcff6f20a809eb21e7efddd2e
zhennann <zhen.nann@icloud.com>
2024-02-15 21:25:17 +0800

    feat: remove monkey

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import 'cabloy-module-api-test-hostsimple-sync';
 import 'cabloy-module-api-test-localeone';
 import 'cabloy-module-api-test-localetwo';
 import 'cabloy-module-api-test-party';
-import 'cabloy-module-api-test-partymonkey-monkey';
+import 'cabloy-module-api-test-partymonkey';
